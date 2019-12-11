8b1d3d47acce33e4b03d584287e3661f09691d3a
nageshgurram12 <nageshgurram12@gmail.com>
2019-12-11 12:19:11 -0500

    Update to MFnet model

--- mypath.py
+++ mypath.py
@@ -4,7 +4,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return os.getenv('PASCAL_DATASET_PATH', '../VOCdevkit/VOC2012/')  # folder that contains VOCdevkit/.
+            return os.getenv('PASCAL_DATASET_PATH', '../datasets/pascal-voc-2012/VOCdevkit/VOC2012/')  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
             return os.getenv('SBD_DATASET_PATH','/path/to/datasets/benchmark_RELEASE/')  # folder that contains dataset/.
         elif dataset == 'cityscapes':
